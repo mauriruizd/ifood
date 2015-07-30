9696642dd938af0ea57802d1c439253b289a5e7b
marcos <marks88martinez@gmail.com>
2015-07-30 10:36:55 -0300

    se a agragado modificaciones en hamburguesas input file

--- resources/views/admin/hamburguesas.blade.php
+++ resources/views/admin/hamburguesas.blade.php
@@ -9,116 +9,8 @@
         <!--panel principal-->
 
 
-        <script type="text/javascript">
-            function dd(){
-                $(".pedido_llegados").draggable({containment:'document', revert:true,
-                    start:function(){
-                        contents = this;
-                    }
-                });
-                $( ".select_ordenes" ).droppable({ accept:'.pedido_llegados',
-                    drop: function(){
-                        $( this ).append(contents.innerHTML);
-                        contents.remove();
-                        $( this ).children().last().addClass('minimizado');
-                        $( this ).children().last().click(function(){
-                            console.log($(this));
-                            rollback($(this));
-                        });
-                    }});
-                $('.btn-delete').on('click', function(){
-                    $(this).closest('.pedido_llegados').remove();
-                });
-                $('.eliminar').on('click', function(){
-                    var ordenes = $(this).closest('.seleccion_ordenes');
-                    ordenes.children('.select_ordenes').children().each(function(index){
-                        rollback( $(this) );
-                    });
-                    ordenes.remove();
-
-                });
-
-            };
-
-            function print( title, w, h) {
-                var left = (screen.width/2)-(w/2);
-                var top = (screen.height/2)-(h/2);
-                return window.open("print.html", title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
-            }
 
 
-            function rollback(element){
-                console.log(element);
-                newDIV = document.createElement("div");
-                newDIV.innerHTML = "<div>" +  element.html() + "</div>";
-                newDIV.className = 'pedido_llegados';
-                element.remove();
-                document.getElementById('cuadro_pedido').insertBefore(newDIV, document.getElementById('cuadro_pedido').firstChild);
-                dd();
-            }
-
-
-            function crear_pedido(){
-                var html = '';
-                html += '<div class="pedido_llegados">';
-                html += '<div>';
-                html += '<div class="titulo_orden">N° de Pedido ' + Math.floor(Math.random()*89999+10000) + ' <i id="down" class="fa fa-toggle-down (alias)"></i> </div><!--titulo_orden-->';
-                html += '<div class="info_box">';
-                html += '<p><i class="bol">Nombre: </i>Marcos Martinez</p> ';
-                html += '<p><i class="bol">Telefono: </i>0978655845</p>';
-                html += '<p><i class="bol">Dirección: </i>Barrio San jose al costado de colegio camino de empedrado a tres cuadras de </p>';
-                html += '<div class="room-box">';
-                html += '<h5 class="text-primary"><strong>Pedido:  Pizza</strong></h5>';
-                html += '<p><span class="text-muted">cantidad :</span> 1 </p>';
-                html += '<p><span class="text-muted">Tamanho :</span> Grande </p>';
-                html += '<p><span class="text-muted">Masa :</span> fina  </p>';
-                html += '<p><span class="text-muted">Sabor :</span> 4 quersos, peperoni</p>';
-                html += '<p><span class="text-muted">Destalle :</span>favor si puede sacar el tomate y sim pimienta</p>';
-                html += '<p><h5><i class="text-primary"><strong>Monto:</strong> </i>180.000 Gs.</h5></p>     ';
-                html += '</div>';
-                html += '<button id="eliminar_box" type="button"class="btn btn-danger btn-delete">';
-                html += '<i class="glyphicon glyphicon-trash"></i>';
-                html += '<span>Eliminar</span>';
-                html += '</button>';
-                html += '</div><!--info_box-->';
-                html += '</div>';
-                html += '</div>';
-                $('#cuadro_pedido').append(html);
-                dd();
-            }
-
-            function crear_orden(){
-                var html = '';
-
-                html+='<div class="seleccion_ordenes">';
-                html += '<div class="orden_cocina">';
-                html += '<p id="text_orden"> Entrada de Cocina N° '+ Math.floor(Math.random()*899+100) +'</p><!--text_orden-->';
-                html += '</div><!--orden_cocina-->';
-                html += ' <div class="select_ordenes">';
-                html += ' </div><!--select_ordenes-->';
-                html += '<button  type="button"class="eliminar btn btn-danger btn-delete" >';
-                html += '<i class="glyphicon glyphicon-trash"></i>';
-                html += '<span>Eliminar</span>';
-                html += '</button>';
-                html +='&nbsp;';
-                html += '<button  onclick="print()"   ontitle="" data-placement="top" data-toggle="tooltip" type="button" data-original-title="Print" class="btn btn-warning">';
-                html += '<i class="fa fa-print"></i>';
-                html += '<span>Imprimir</span>';
-                html += '</button>';
-                html += '<div id="espacio"></div>';
-                html += '</div>';
-                $('#panel-body').append(html);
-                dd();
-
-            }
-            $(document).ready(function(){
-                dd();
-            });
-
-
-
-
-        </script>
 
 
         <div id='bok'class="col-lg-12"><!--blog de pedido-->
@@ -165,6 +57,15 @@
                                             <br>
                                             <br>
 
+                                            <!--******************************-->
+                                                <div class="form-group">
+                                                    <input id="file-1" type="file" class="file" multiple=true data-preview-file-type="any">
+                                                </div>
+                                                <br>
+                                                <br>
+                                            <!--****************************-->
+
+
                                             <button type="submit" class="btn btn-danger">Guardar</button>
                                         </div><!--form_box-->
 
@@ -188,6 +89,7 @@
                                                         <th>Detaller</th>
                                                         <th></th>
                                                         <th></th>
+                                                        <th></th>
                                                         <th >Activar</th>
 
                                                     </tr>
@@ -197,6 +99,7 @@
                                                         <td>Clasica</td>
                                                         <td>4 queso</td>
                                                         <td>peperoni, tomate, salsa, queso</td>
+                                                        <td align="center" class="img_promo"><img src="{{URL::to('admin/imagen/promo.jpeg')}}"></td>
                                                         <td align="center"><button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button></td>
                                                         <td align="center"><button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button></td>
                                                         <td align="center">
@@ -209,13 +112,13 @@
                                                                 </div>
                                                             </section>
                                                         </td>
-
                                                     </tr>
 
                                                     <tr class="">
                                                         <td>de la casa</td>
                                                         <td>clasicas</td>
                                                         <td>clasicas</td>
+                                                        <td align="center" class="img_promo"><img src="{{URL::to('admin/imagen/promo.jpeg')}}"></td>
                                                         <td align="center"><button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button></td>
                                                         <td align="center"><button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button></td>
                                                         <td align="center">
@@ -384,7 +287,6 @@
     </div><!--blog de pedido fin-->
 
     <!--****************************************************************-->
-
 
 
 
