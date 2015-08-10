e0919b7dd73a5bb9318e71597ec24a1ebf9dcd4d
marcos <marks88martinez@gmail.com>
2015-08-10 10:43:20 -0300

    trabajando con create usuario

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,28 @@
+<?php namespace App\Http\Middleware;
+use Illuminate\Contracts\Auth\Guard;
+use Closure;
+use Session;
+
+class Admin {
+	protected $auth;
+	public function __construct(){
+		$this->auth =$auth;
+	}
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		if($this->auth->user()->codigo !=1){
+			Session::flash('mensage-error', 'Sin Privilegios');
+			return redirect()->to('/panel');
+		}
+		return $next($request);
+	}
+
+}
