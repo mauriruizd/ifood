ca16f2b54f457e3c4597525c5033e86556c020dc
marcos <marks88martinez@gmail.com>
2015-08-10 10:06:23 -0300

    conexxion de la base de datos

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'     => '192.168.1.196',
+			'host'     => 'localhost',
 			'database' => 'delcheff',
 			'username' => 'root',
 			'password' => '',
