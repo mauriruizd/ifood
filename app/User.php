0dfde7c33fc3eb0f517ba853afcc090a7b001f5b
marcos <marks88martinez@gmail.com>
2015-08-10 10:03:16 -0300

    formulario usuario

--- app/User.php
+++ app/User.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
 
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
 
@@ -16,13 +18,16 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 * @var string
 	 */
 	protected $table = 'users';
+	protected $primaryKey ='codigo';
+	public $timestamps = false;
 
 	/**
 	 * The attributes that are mass assignable.
 	 *
 	 * @var array
 	 */
-	protected $fillable = ['name', 'email', 'password'];
+	protected $fillable = ['name', 'email', 'password','persona_empresa_codigo','roles'];
+	//protected $fillable = ['login', 'clave'];
 
 	/**
 	 * The attributes excluded from the model's JSON form.
@@ -30,5 +35,13 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 * @var array
 	 */
 	protected $hidden = ['password', 'remember_token'];
+	protected $dates = ['deleted_at'];
+
+	public function setPasswordAttributr($valor){
+		if(!empty($valor)){
+			$this->attributes['password']=\Hash::make($valor);
+		}
+
+	}
 
 }
