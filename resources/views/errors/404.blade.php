b7651fc9c415088e8b54d9554badcd5ab05f962a
mauri.rd <mauri.rd@gmail.com>
2015-08-17 15:05:38 -0400

    Agregadas categorias

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@extends('user')
+@section('page-content')
+    <div id="center">
+        <img src="{{ URL::to('img/cheff-triste.png') }}" alt="Error">
+        <h1>Lo sentimos, la página que estás buscando no existe</h1>
+    </div>
+@stop
