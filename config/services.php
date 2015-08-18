3969125a7db38708cce50a4398a4114636663077
mauri.rd <mauri.rd@gmail.com>
2015-08-18 13:10:36 -0400

    Cambiado redireccionamiento al hacer login con facebook y google+

--- config/services.php
+++ config/services.php
@@ -37,13 +37,13 @@ return [
 	'facebook' => [
 		'client_id' => '364497073744229',
 		'client_secret' => '373504591408690035861a5bc15980c3',
-		'redirect' => 'http://localhost:8000/facebook/login',
+		'redirect' => 'http://www.delcheff.com/facebook/login',
 	],
 
 	'google' => [
 		'client_id' => '514041287979-icq396nmn5qvb8c95q4jufgok7h5lqqs.apps.googleusercontent.com',
 		'client_secret' => 'jEX8cQFlrRFrGt6bel1jgjUd',
-		'redirect' => 'http://localhost:8000/google/login',
+		'redirect' => 'http://www.delcheff.com/google/login',
 	],
 
 ];
