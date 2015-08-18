a4d0ce2644c0051ab267e85cbe7596da3d7e8eb5
marcos <marks88martinez@gmail.com>
2015-08-18 11:41:20 -0300

    controlador de detalle

--- app/Http/Controllers/PizzaControllerDetalle.php
+++ app/Http/Controllers/PizzaControllerDetalle.php
@@ -3,7 +3,9 @@
 use App\TamanhoPizza;
 use App\EspecialidadPizza;
 use App\MasaPizza;
+use App\DetallePizza;
 use App\Http\Requests;
+
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
@@ -18,19 +20,10 @@ class PizzaControllerDetalle extends Controller {
 	 *
 	 * @return Response
 	 */
-	public function index()
-	{
-		//
-	}
+	protected function crearSelect(){
+		$pizzaEspecie = EspecialidadPizza::where('empresa_codigo','=', Auth::user()->persona_empresa_codigo)
+			->get();
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		$pizzaEspecie = EspecialidadPizza::all();
 		$selectEspecie = array();
 		foreach ($pizzaEspecie as $pizzaEspecies){
 			$selectEspecie[$pizzaEspecies->codigo] = $pizzaEspecies->nombre;
@@ -45,8 +38,40 @@ class PizzaControllerDetalle extends Controller {
 		foreach($pizzaMasa as $pizzaMasas){
 			$selectMasa[$pizzaMasas->codigo] = $pizzaMasas->nombre;
 		}
+		return ['selectTamanho'=>$selectTamanho,'selectMasa'=>$selectMasa,'selectEspecie'=>$selectEspecie];
+	}
 
-		return view('admin.PizzaDetalle', compact('pizzaEspecie','selectEspecie','pizzaTamanho','selectTamanho','pizzaMasa','selectMasa'));
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+
+		$detaPizza = DetallePizza::join('cat_pizza_especialidades', 'cat_pizza_especialidades.codigo', '=', 'cat_pizza_detalles.cat_pizza_esp_codigo')
+			->join('cat_pizza_tipo_masa', 'cat_pizza_tipo_masa.codigo', '=', 'cat_pizza_detalles.cat_pizza_masa_codigo')
+			->join('cat_pizza_tamanhos', 'cat_pizza_tamanhos.codigo', '=', 'cat_pizza_detalles.cat_pizza_tamanho_codigo')
+			->select('cat_pizza_tipo_masa.nombre as masa_nombre', 'cat_pizza_tamanhos.nombre as tamanho_nombre', 'cat_pizza_detalles.precio', 'cat_pizza_detalles.codigo as config_pizza',
+				'cat_pizza_tamanhos.cant_sabores', 'cat_pizza_tamanhos.cant_porcion', 'cat_pizza_detalles.cat_pizza_esp_codigo as esp_codigo','cat_pizza_especialidades.nombre as especialidad_nombre',
+				'cat_pizza_especialidades.empresa_codigo','cat_pizza_detalles.estado','cat_pizza_detalles.codigo')
+			->where('cat_pizza_especialidades.empresa_codigo', '=', Auth::user()->persona_empresa_codigo)
+
+			->get();
+
+
+
+			$selector = $this->crearSelect();
+			$selectEspecie=$selector['selectEspecie'];
+			$selectMasa =$selector['selectMasa'];
+			$selectTamanho=$selector['selectTamanho'];
+
+		return view('admin.PizzaDetalle', compact('detaPizza','pizzaEspecie','selectEspecie','pizzaTamanho','selectTamanho','pizzaMasa','selectMasa'));
 	}
 
 	/**
@@ -54,8 +79,19 @@ class PizzaControllerDetalle extends Controller {
 	 *
 	 * @return Response
 	 */
-	public function store()
+	public function store(Request $request)
 	{
+		DetallePizza::create([
+			'cat_pizza_esp_codigo'=>$request['especialidad'],
+			'cat_pizza_tamanho_codigo'=>$request['tamanho'],
+			'cat_pizza_masa_codigo'=>$request['masa'],
+			'precio'=>$request['precio'],
+			'estado'=>1,
+
+		]);
+
+		Session::flash('message','Especialidad creado exitosamente');
+		return Redirect::to('/PizzaControlDetalle/create');
 
 	}
 
@@ -76,9 +112,15 @@ class PizzaControllerDetalle extends Controller {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function edit($id)
+	public function edit($codigo)
 	{
-		//
+		$selector = $this->crearSelect();
+		$selectEspecie=$selector['selectEspecie'];
+		$selectMasa =$selector['selectMasa'];
+		$selectTamanho=$selector['selectTamanho'];
+
+		$detaPizza = DetallePizza::find($codigo);
+		return view('usuario.edit_detalle',['detaPizza'=>$detaPizza, 'selectEspecie'=>$selectEspecie, 'selectMasa'=>$selectMasa,'selectTamanho'=>$selectTamanho]);
 	}
 
 	/**
@@ -87,9 +129,20 @@ class PizzaControllerDetalle extends Controller {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function update($id)
+	public function update($codigo)
+
 	{
-		//
+
+	}
+	public function update_estado($codigo)
+	{
+		$estado = DetallePizza::find($codigo);
+		$estado->estado=$estado->estado?0:1;
+		$estado->save();
+		Session::flash('message','Estado actualizado exitosamente');
+		return Redirect::to('/PizzaControlDetalle/create');
+
+
 	}
 
 	/**
@@ -98,9 +151,11 @@ class PizzaControllerDetalle extends Controller {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function destroy($id)
+	public function destroy($codigo)
 	{
-		//
+		DetallePizza::destroy($codigo);
+		Session::flash('message','Detaller eliminado correctamente');
+		return Redirect::to('/PizzaControlDetalle/create');
 	}
 
 }
