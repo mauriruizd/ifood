57689a698f99d2464e3a1abd20e46b254d78cdb1
marcos <marks88martinez@gmail.com>
2015-08-20 16:53:03 -0300

    creacion de lomito menu

---
+++ resources/views/usuario/lomitomenu.blade.php
@@ -0,0 +1,7 @@
+<div class="panel-body">
+    <a href="{{URL::to('/LomitoControl/create')}}"><button type="button" class="btn btn-shadow btn-warning">Producto</button></a>
+    <a href="{{URL::to('/EspecialidadControlLomito/create')}}"><button type="button" class="btn btn-shadow btn-warning">Especialidad</button></a>
+
+    <a href="{{URL::to('/ControlExtras/create')}}"><button type="button" class="btn btn-shadow btn-warning">Extras</button></a>
+
+</div>
