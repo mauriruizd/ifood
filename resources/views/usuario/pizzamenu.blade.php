9880409c3dd21adf48791c2839146b5d61e32083
marcos <marks88martinez@gmail.com>
2015-08-20 16:54:33 -0300

    Masa

--- resources/views/usuario/pizzamenu.blade.php
+++ resources/views/usuario/pizzamenu.blade.php
@@ -1,10 +1,10 @@
 <div class="panel-body">
-    <a href="{{URL::to('PizzaControlProducto/create')}}"><button type="button" class="btn btn-shadow btn-warning">Sabor</button></a>
+    <a href="{{URL::to('PizzaControlProducto/create')}}"><button type="button" class="btn btn-shadow btn-warning">Producto</button></a>
     <a href="{{URL::to('PizzaControl/create')}}"><button type="button" class="btn btn-shadow btn-warning">Especialidad</button></a>
     <a href="{{URL::to('PizzaControlTamanho/create')}}"><button type="button" class="btn btn-shadow btn-warning">Tamanhos</button></a>
     <a href="{{URL::to('PizzaControlMasa/create')}}"><button type="button" class="btn btn-shadow btn-warning">Masa</button></a>
     <a href="{{URL::to('PizzaControlDetalle/create')}}"><button type="button" class="btn btn-shadow btn-warning">Detaller</button></a>
 
-    <a href="{{URL::to('PizzaControl/globales')}}"><button type="button" class="btn btn-shadow btn-warning">Extras</button></a>
+    <a href="{{URL::to('/ControlExtras/create')}}"><button type="button" class="btn btn-shadow btn-warning">Extras</button></a>
 
 </div>
