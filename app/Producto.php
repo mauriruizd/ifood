8190bd5d7185226c10c1670bf5f8147342c123af
marcos <marks88martinez@gmail.com>
2015-08-21 16:37:14 -0300

    vista de lomito

--- app/Producto.php
+++ app/Producto.php
@@ -8,6 +8,6 @@ class Producto extends Model {
 	protected $primaryKey ='codigo';
 	
 	public $timestamps = false;
-	protected $fillable = ['denominacion','imagen_url','descripcion','categoria_codigo','subcategoria_codigo','empresa_codigo','estado'];
+	protected $fillable = ['denominacion','imagen_url','descripcion','categoria_codigo','subcategoria_codigo','empresa_codigo','estado','precio'];
 
 }
