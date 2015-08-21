8e98a68841e839c1a259fdaa64b3ad29f46bafcf
marcos <marks88martinez@gmail.com>
2015-08-21 16:36:47 -0300

    controller de lomito

--- app/Http/Controllers/LomitoController.php
+++ app/Http/Controllers/LomitoController.php
@@ -3,19 +3,34 @@
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Producto;
+use App\Subcategoria;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use Intervention\Image\Facades\Image;
+
 use Session;
 use Redirect;
 use Auth;
 
-class LomitoController extends Controller {
+class LomitoController extends Controller{
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
+	protected function crearImagen($inputImagen){
+
+
+		$imagenInput = $inputImagen;
+		$nombre_image = $imagenInput->getClientOriginalName();
+		$imagenInput->move('img/productos/'.Auth::user()->persona_empresa_codigo,$nombre_image);
+		$imagen_final = 'img/productos/'.Auth::user()->persona_empresa_codigo.'/'.$nombre_image;
+
+		$int_imagen= Image::make($imagen_final);
+		$int_imagen->resize(568,null, function($constraint){
+			$constraint->aspectRatio();
+		});
+		$int_imagen->save($imagen_final);
+		return $imagen_final;
+	}
+
 	public function index()
 	{
 		//
@@ -28,9 +43,25 @@ class LomitoController extends Controller {
 	 */
 	public function create()
 	{
+		$lomitoProducto = Producto::join('subcategorias','subcategorias.codigo','=','productos.subcategoria_codigo')
+			->select('subcategorias.nombre as subcat_nombre','productos.denominacion','productos.descripcion','productos.precio','productos.imagen_url','productos.estado','productos.codigo')
+			->where('subcategorias.categoria_codigo', '=', 2)
+			->where('subcategorias.empresa_codigo','=', Auth::user()->persona_empresa_codigo)->paginate(4);
 
-		$lomito = Producto::where('empresa_codigo', '=', Auth::user()->persona_empresa_codigo);
-		return view('admin.LomitoView', compact('lomito'));
+			//->get();
+
+			//->where('categoria_codigo', '=', 2)->paginate(4);
+
+		$lomito = Subcategoria::where('empresa_codigo', '=', Auth::user()->persona_empresa_codigo)
+			->where('categoria_codigo', '=', 2)
+			->get();
+
+		$selectLomito = array();
+		foreach ($lomito as $lomitos) {
+			$selectLomito[$lomitos->codigo] = $lomitos->nombre;
+		}
+
+		return view('admin.LomitoView', compact('lomito','selectLomito','lomitoProducto'));
 	}
 
 	/**
@@ -40,16 +71,24 @@ class LomitoController extends Controller {
 	 */
 	public function store(Request $request)
 	{
+
+	$imagen_final = $this->crearImagen(Input::file('image'));
+
+
+
 		Producto::create([
 			'denominacion'=>$request['nombre'],
 			'subcategoria_codigo'=>$request['especialidad'],
 			'categoria_codigo'=>2,
 			'descripcion'=>$request['descrip'],
 			'empresa_codigo'=>Auth::user()->persona_empresa_codigo,
-			//'imagen_url'=>$imagen_final,
+			'imagen_url'=>$imagen_final,
+			'precio'=>$request['precio'],
 			'estado'=>1,
 
 		]);
+		Session::flash('message','Producto creado correctamente');
+		return Redirect::to('/LomitoControl/create');
 	}
 
 	/**
@@ -84,6 +123,17 @@ class LomitoController extends Controller {
 	{
 		//
 	}
+	public function update_estado($codigo)
+	{
+
+		$estado = Producto::find($codigo);
+		$estado->estado=$estado->estado?0:1;
+		$estado->save();
+		Session::flash('message','Estado actualizado exitosamente');
+		return Redirect::to('/LomitoControl/create');
+
+
+	}
 
 	/**
 	 * Remove the specified resource from storage.
@@ -91,9 +141,11 @@ class LomitoController extends Controller {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function destroy($id)
+	public function destroy($codigo)
 	{
-		//
+		Producto::destroy($codigo);
+		Session::flash('message','Producto eliminado correctamente');
+		return Redirect::to('/LomitoControl/create');
 	}
 
 }
